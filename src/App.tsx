ff0ff9d20309ce8a75e2391728bd686cfc410853
Gustavo Saboya <saboya.gutavo@gmail.com>
2022-06-10 21:25:47 -0300

    Implemented NewRoom page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Home } from "./pages/Home";
+// import { Home } from "./pages/Home";
+import { NewRoom } from './pages/NewRoom';
 import './styles/global.scss';
 
 export function App() {
   return (
     <>
-      <Home/>
+      <NewRoom/>
     </>
   );
 }
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,32 @@
+import { BannerAside } from "../components/BannerAside";
+import { Button } from "../components/Button";
+
+import logoImg from '../assets/images/logo.svg';
+
+import '../styles/home.scss';
+
+export function NewRoom() {
+  return (
+    <div id='new-room-page'>
+      <BannerAside />
+
+      <main>
+        <div className='main-content'>
+          <img id='logo' src={logoImg} alt='Letmeask' draggable='false' />
+
+          <h2>Crie uma nova sala</h2>
+
+          <form>
+            <input type='text' placeholder='Nome da sala' value='' />
+            <Button type='submit'>Criar sala</Button>
+          </form>
+
+          <p className='join-room-redirect'>
+            Quer entrar em uma sala já existente?&nbsp;
+            <a href='#'>Clique aqui</a>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
