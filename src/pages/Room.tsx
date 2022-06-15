07a1ed12cff74663b34d12990258c68e8a5bdf0a
Gustavo Saboya <saboya.gustavo@gmail.com>
2022-06-14 23:30:52 -0300

    Structured the Room page.

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,3 +1,35 @@
+import { Button } from "../components/Button";
+
 export function Room() {
-  return <h1>Hello, Room</h1>
+  return (
+    <div>
+      <header>
+        <div>
+          <img id='logo' src='' alt='' />
+
+          <Button>
+            <img src='' alt='' />
+            codigo da sala
+          </Button>
+        </div>
+      </header>
+
+      <main>
+        <h1>Sala React</h1>
+        <form>
+          <textarea style={{resize: 'none'}} />
+
+          <div>
+            <span>
+              Para enviar uma pergunta, <button>faça seu login</button>.
+            </span>
+            <div>
+              <img src='' alt='' />
+              <span></span>
+            </div>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
 }
